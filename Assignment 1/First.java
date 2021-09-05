e9290bc68b7c5e837758afe484a6c6cc8ba86dd9
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-09-05 15:32:29 +0530

    Assignment 1 done

---
+++ Assignment 1/First.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class First{
+    public static void main(String[] args){
+    Scanner Input=new Scanner(System.in);
+    int number=Input.nextInt();
+    if(number%2==0){
+        System.out.println("Number is Even");
+    }
+    else{
+        System.out.println("Number is Odd");
+    }
+    }
+
+}
