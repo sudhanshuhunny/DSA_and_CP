344fbd21ebafb8d93bb7e105d2d59a2f9a627fa6
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-12-30 13:56:33 +0530

    Array 2 basic done

--- Assignments and Solutions/5-Arrays/src/Build_Array_from_permutation.java
+++ Assignments and Solutions/5-Arrays/src/Build_Array_from_permutation.java
@@ -12,8 +12,20 @@ ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]]
     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
     = [0,1,2,4,5,3]
  */
-
 public class Build_Array_from_permutation {
-int[] ar={'5'};
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        System.out.println(buildArray(nums));
 
+    }
+
+    public static int[] buildArray(int[] nums) {
+
+        int size = nums.length;
+        int[] ans = new int[size];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
 }
---
+++ Assignments and Solutions/5-Arrays/src/Concatenation_of_array.java
@@ -0,0 +1,40 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+Example 2:
+
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+ */
+
+
+import java.util.ArrayList;
+
+public class Concatenation_of_array {
+    public static void main(String[] args) {
+        int[] nums={1,2,1};
+        System.out.println(solution(nums));
+    }
+    public static int[] solution(int[] nums){
+        ArrayList<> ar=new ArrayList<>();
+        ar.add(5);
+     return ar;
+    }
+
+}
