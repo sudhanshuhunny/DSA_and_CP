3526ca0234d419b033f8a056a8f4f92cf1d4a668
sudhanshuhunny <sudhanshusengh@gmail.com>
2021-12-31 23:01:58 +0530

    Modified Two Sums

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -505,12 +505,10 @@ public int[] twoSum(int[] nums, int target) {
     int[] ans=new int[2];
     int k=0;
     for(int i=0;i<nums.length;i++){
-        for(int j=i;j<nums.length;j++){
+        for(int j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]==target){
                 ans[k]=i;
-                ++k;
-                ans[k]=j;
-                k++;
+                ans[k+1]=j;
 
             }
 
