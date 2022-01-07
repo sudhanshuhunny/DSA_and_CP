466d7f49a665957f0d024edab5f9e178a2b3fd94
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-07 12:41:28 +0530

    Updated removeDupliactes and Added first and last position in array

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1033,13 +1033,117 @@ nums is sorted in non-decreasing order.
  */
 
     public int removeDuplicates(int[] nums) {
-        int count=1;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]!=nums[i+1]){
-                count++;
-            }
-        }
-        return count;
+        int ans = 0;
+        for (int number : nums)
+            if (ans == 0 || number > nums[ans-1])
+                nums[ans++] = number;
+        return ans;
     }
+/*34. Find First and Last Position of Element in Sorted Array
+    Medium
 
+8610
+
+        264
+
+    Add to List
+
+            Share
+    Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+    If target is not found in the array, return [-1, -1].
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+
+
+    Example 1:
+
+    Input: nums = [5,7,7,8,8,10], target = 8
+    Output: [3,4]
+    Example 2:
+
+    Input: nums = [5,7,7,8,8,10], target = 6
+    Output: [-1,-1]
+    Example 3:
+
+    Input: nums = [], target = 0
+    Output: [-1,-1]
+
+
+    Constraints:
+
+            0 <= nums.length <= 105
+            -109 <= nums[i] <= 109
+    nums is a non-decreasing array.
+*/
+   /*     int size=nums.length;
+        int mid=(size+1)/2;
+        int start=0;
+        int end=size;
+        int [] ans=int[2]
+        if(target>=nums[mid]){
+
+            if(mid!=start){
+            if(nums[mid]==target){
+                start=mid;
+                ans[0]=start;
+            }
+            else{
+                ans[0]
+            }}
+            else{
+                if(nums[mid]==target){
+                    end=mid;
+                    ans[1]=end;
+                }
+                else{
+                    ans[1]=-1;
+                }
+            }
+            mid++;
+
+        }
+        else{
+           start=0;
+           if(nums)
+        }
+    }*/
+public int[] searchRangei(int[] nums, int target) {
+
+    int[] ans = new int[2];
+    ans[0] = findFirst(nums, target);
+    ans[1] = findLast(nums, target);
+    return ans;
+}
+private int findFirst(int[] nums, int target){
+    int i = -1;
+    int start = 0;
+    int end = nums.length - 1;
+    while(start <= end){
+        int mid = (start + end) / 2;
+        if(nums[mid] >= target){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+        if(nums[mid] == target) i = mid;
+    }
+    return i;
+}
+    private int findLast(int[] nums, int target){
+        int i = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] <= target){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+            if(nums[mid] == target) i = mid;
+        }
+        return i;
+    }
 }
