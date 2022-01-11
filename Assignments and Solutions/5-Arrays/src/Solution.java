072e0ba0a4b626d33d08e3e2cf812e7b03a79bf4
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-11 23:07:44 +0530

    Added SortCOlOR

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1299,3 +1299,53 @@ Each string does not contain leading zeros except for the zero itself.
         return ans.reverse().toString();
     }
 }
+
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+ */
+
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int i = 0;
+        while( i <= end ) {
+            if( nums[i] == 0 )
+                swapy(nums, start++, i++);
+            else if( nums[i] == 2)
+                swapy(nums, end--, i);
+            else
+                i++;
+        }
+    }
+
+    public void swapy(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
