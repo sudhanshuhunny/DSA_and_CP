01f6b6328b9613cf268597f8a2922d5ec3f55200
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-13 00:02:11 +0530

    Rotate by K Array  Added

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1383,4 +1383,72 @@ Constraints:
         }
         return true;
     }
+
+/*
+189. Rotate Array
+Medium
+
+7337
+
+1143
+
+Add to List
+
+Share
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+Example 2:
+
+Input: nums = [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation:
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-231 <= nums[i] <= 231 - 1
+0 <= k <= 105
+
+
+Follow up:
+
+Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
+Could you do it in-place with O(1) extra space?
+ */
+public void rotate(int[] nums, int k) {
+
+    if(nums == null || nums.length < 2){
+        return;
+    }
+
+    k = k % nums.length;
+    swap(nums, 0, nums.length - k - 1);
+    swap(nums, nums.length - k, nums.length - 1);
+    swap(nums, 0, nums.length - 1);
+
+}
+
+    private void swap(int[] nums, int i, int j){
+        int temp = 0;
+        while(i < j){
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
 }
