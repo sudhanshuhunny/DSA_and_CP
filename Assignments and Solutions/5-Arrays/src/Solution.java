dddf54a5f1b0e05901810ae8ba477f975ecf4d5e
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-17 23:32:36 +0530

    Added SetZeroes

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1554,4 +1554,25 @@ public int[] productExceptSelf(int[] nums) {
                     row[i] = row[size - i - 1] ^= 1;
         return image;
     }
+    public void setZeroes(int[][] matrix) {
+        int x = 1;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0)
+                x = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (x == 0)
+                matrix[i][0] = 0;
+        }
+    }
 }
