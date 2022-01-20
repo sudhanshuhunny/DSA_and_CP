db95c2b6aa4d6017656288eb2568a82d7b62c6b5
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-20 23:33:19 +0530

    AddToArrayForm

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1593,4 +1593,23 @@ public int[] productExceptSelf(int[] nums) {
             ans[i] = i * 2 - n + 1;
         return ans;
     }
+
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> ans =new ArrayList<>();
+
+        for(int i=num.length-1;i>=0;i--){
+            int x=num[i];
+            int sum=x+k;
+            int rem=sum%10;
+            ans.add(0,rem);
+            k=sum/10;
+        }
+
+        while(k>0){
+            ans.add(0,k%10);
+            k/=10;
+        }
+        return ans;
+
+    }
 }
