8ef58c0666eb97cd5ea97a480d739652e79a42b6
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-20 23:32:41 +0530

    Added sum zero

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1587,5 +1587,10 @@ public int[] productExceptSelf(int[] nums) {
         }
         return x == 1;
     }
-
+    public int[] sumZero(int n) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; ++i)
+            ans[i] = i * 2 - n + 1;
+        return ans;
+    }
 }
