156872dfdf35618e444936b775dc5ffb8abfe5da
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-25 23:34:51 +0530

    Added find rotation matrix

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -1846,4 +1846,65 @@ public int lengthOfLongestSubstring(String s) {
     }
     return length;
 }
+/*
+Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
+
+
+
+Example 1:
+
+
+Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+Output: true
+Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+Example 2:
+
+
+Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
+Output: false
+Explanation: It is impossible to make mat equal to target by rotating mat.
+Example 3:
+
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
+Output: true
+Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
+
+
+Constraints:
+
+n == mat.length == target.length
+n == mat[i].length == target[i].length
+1 <= n <= 10
+mat[i][j] and target[i][j] are either 0 or 1.
+ */
+public boolean findRotation(int[][] mat, int[][] target) {
+    int size = mat.length;
+    int[] res[] = new int[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            res[i][j] = mat[size - 1 - j][i];
+        }
+    }
+
+    int[] res2[] = new int[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            res2[i][j] = res[size - 1 - j][i];
+        }
+    }
+
+    int[] res3[] = new int[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            res3[i][j] = res2[size - 1 - j][i];
+        }
+    }
+
+    //compare to 90,180,270 and itself
+    if(Arrays.deepEquals(target, res) || Arrays.deepEquals(target, res2) || Arrays.deepEquals(target, res3) || Arrays.deepEquals(target, mat) ){
+        return true;
+    }
+    return false;
+}
 }
