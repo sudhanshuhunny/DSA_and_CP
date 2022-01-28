a80dc577d933141c39ce4513bc007d2458437f3b
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-28 23:29:47 +0530

    Solution Cells with odd Integer

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -2014,6 +2014,20 @@ Constraints:
 
  */
 
-
+    public int oddCells(int m, int n, int[][] indices) {
+        int count = 0;
+        int row[] = new int[m];
+        int col[] = new int[n];
+        for (int ans[] : indices) {
+            row[ans[0]]++;
+            col[ans[1]]++;
+        }
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if ((row[i] + col[j]) % 2 != 0)
+                    count++;
+            }
+        return count;
+    }
 
 }
