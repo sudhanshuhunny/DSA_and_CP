00e2a7d58154d2eab3cc875d1439f1e04edca500
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-28 23:30:57 +0530

    Solution createtargetArray

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -2088,4 +2088,17 @@ No
 Problems
 
  */
+public int[] createTargetArray(int[] nums, int[] index) {
+    ArrayList<Integer> ans = new ArrayList<Integer>();
+    for(int i=0;i<nums.length;i++)
+    {
+        ans.add(index[i],nums[i]);
+    }
+    int target[] = new int[nums.length];
+    for(int i=0;i<nums.length;i++)
+    {
+        target[i] = ans.get(i);
+    }
+    return target;
+}
 }
