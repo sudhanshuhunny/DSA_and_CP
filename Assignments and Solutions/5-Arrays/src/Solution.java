1451174945bdf0bc75160bf4ae832fb3f18bbc7b
sudhanshuhunny <sudhanshusengh@gmail.com>
2022-01-30 23:30:44 +0530

    Trying Branch method and Added 3sum Solution

--- Assignments and Solutions/5-Arrays/src/Solution.java
+++ Assignments and Solutions/5-Arrays/src/Solution.java
@@ -2161,4 +2161,26 @@ public int diagonalSfum(int[][] mat) {
     return size % 2 == 0 ? res : res - mat[size/2][size/2];
 
 }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> ans = new HashSet<>();
+        if(nums.length == 0)
+            return new ArrayList<>(ans);
+
+        Arrays.sort(nums);
+
+        for(int i=0; i< nums.length-2; i++){
+            int j = i+1;
+            int k= nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0)
+                    ans.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+                else if(sum > 0)
+                    k--;
+                else if(sum <0)
+                    j++;
+            }
+        }
+        return new ArrayList<>(ans);
+    }
 }
